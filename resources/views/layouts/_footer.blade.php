1c2d79bd2ce056b7eba17393ab2636838e6668eb
vagrant <vagrant@homestead>
2023-04-17 14:49:40 +0000

    change style

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="footer">
   <div class="container">
     <p class="float-start">
-      Designed by<a href="https://www.linkedin.com/in/%E6%A5%A0%E5%BD%AC-%E5%88%98-4b3a22272/" target="_blank">Nanbin</a> <span style="color: #e27575;font-size: 14px;">❤</span>
+      Designed by <a href="https://www.linkedin.com/in/%E6%A5%A0%E5%BD%AC-%E5%88%98-4b3a22272/" target="_blank">Nanbin</a> <span style="color: #e27575;font-size: 14px;">❤</span>
     </p>
 
     <p class="float-end"><a href="18811603613@163.com">Contact us</a></p>
