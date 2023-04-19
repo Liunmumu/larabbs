4005ed6848b9ca503ee5647ce46f25645f6d1f6e
Liunmumu <1060556579@qq.com>
2023-04-19 03:50:31 +0000

    login logout register

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -16,9 +16,31 @@
 
       <!-- 右侧菜单栏 -->
       <ul class="navbar-nav navbar-right">
-        <!-- 设置跳转 -->
-        <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">Sign in</a></li>
-        <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">Sigh up</a></li>
+        <!-- 判断登录状态 -->
+        @guest
+          <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">Sign in</a></li>
+          <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">Sigh up</a></li>
+        @else
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown"
+              aria-haspopup="true" aria-expanded="false">
+              <img src="https://cdn.learnku.com/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/60/h/60"
+                class="img-responsive img-circle" width="30px" height="30px">
+              {{ Auth::user()->name }}
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="">Personal center</a>
+              <a class="dropdown-item" href="">Edit information</a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" id="logout" href="#">
+                <form action="{{ route('logout') }}" method="POST">
+                  {{ csrf_field() }}
+                  <button class="btn btn-block btn-danger" type="submit" name="button">Sign out</button>
+                </form>
+              </a>
+            </div>
+          </li>
+        @endguest
       </ul>
     </div>
   </div>
