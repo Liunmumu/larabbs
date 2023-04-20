fefe240c7e5a356b2403706ba57c93879021b003
Liunmumu <1060556579@qq.com>
2023-04-20 13:07:34 +0000

    Show user information

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -12,11 +12,12 @@
           src="https://cdn.learnku.com/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/600/h/600"
           alt="{{ $user->name }}">
         <div class="card-body">
-          <h5><strong>Personal profile</strong></h5>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+          <h5><strong>Introduction</strong></h5>
+          <p>{{ $user->introduction }}</p>
           <hr>
           <h5><strong>Registered at</strong></h5>
-          <p>January 01 2023</p>
+          {{ dd($user->created_at) }}
+          <p>{{ $user->created_at->diffForHumans() }}</p>
         </div>
       </div>
     </div>
