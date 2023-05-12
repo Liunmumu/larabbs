9e9b10f110c2f3299f119182a05c3ee51fe69dcc
Liunmumu <1060556579@qq.com>
2023-05-12 11:39:40 +0000

    replies

--- app/Models/User.php
+++ app/Models/User.php
@@ -54,4 +54,10 @@ class User extends Authenticatable implements MustVerifyEmail
     {
         return $this->id == $model->user_id;
     }
+
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
+    }
+
 }
