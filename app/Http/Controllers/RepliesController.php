a3cfab443f78a8fe2b08f4a78adf28fe304d55e9
Liunmumu <1060556579@qq.com>
2023-05-14 15:58:56 +0000

    fix bugs

--- app/Http/Controllers/RepliesController.php
+++ app/Http/Controllers/RepliesController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\ReplyRequest;
 use Illuminate\Support\Facades\Auth;
+use App\Models\Link;
 
 class RepliesController extends Controller
 {
