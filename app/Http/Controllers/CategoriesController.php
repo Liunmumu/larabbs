4fc8cfc130feebd7d8563a734ef34ecb5485d0ef
Liunmumu <1060556579@qq.com>
2023-05-14 15:54:53 +0000

    fix bugs

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Topic;
 use App\Models\Category;
+use App\Models\User;
 
 class CategoriesController extends Controller
 {
