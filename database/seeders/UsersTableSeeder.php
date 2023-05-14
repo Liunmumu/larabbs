9009e21e9444835ba733df77682d46555375dd2e
Liunmumu <1060556579@qq.com>
2023-05-14 10:13:05 +0000

    give permission

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -22,14 +22,15 @@ class UsersTableSeeder extends Seeder
         $user = User::find(2);
         $user->name = '6666';
         $user->email = '1060556579@qq.com';
-        $user->avatar = 'http://happybbs.herokuapp.com//uploads/images/avatars/202304/24/2_1682341202_QHto0ANxeZ.png';
+        $user->avatar = 'https://cdn.learnku.com/uploads/images/201710/14/1/ZqM7iaP4CR.png';
         $user->save();
 
         // 初始化用户角色，将 1 号用户指派为『站长』
+        $user = User::find(2);
         $user->assignRole('Founder');
 
         // 将 2 号用户指派为『管理员』
-        $user = User::find(2);
+        $user = User::find(1);
         $user->assignRole('Maintainer');
     }
 }
