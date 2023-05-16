3e293a367198025199e4a3df4354bcafa6dfb87d
Liunmumu <1060556579@qq.com>
2023-05-16 11:27:51 +0000

    fix bugs

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -11,7 +11,7 @@ use App\Models\Link;
 class CategoriesController extends Controller
 {
     //
-    public function show(Category $category, Request $request, Topic $topic, User $user)
+    public function show(Category $category, Request $request, Topic $topic, User $user, Link $link)
     {
         // 读取分类 ID 关联的话题，并按每 20 条分页
         $topics = $topic->withOrder($request->order)
